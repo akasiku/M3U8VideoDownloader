2ce96c42c9505f916686b2ef364909157314e602
jiangjie <1349892584qq@gmail.com>
2023-02-12 00:41:05 +0900

    Initialization folder

---
+++ 视频解密器.py
@@ -0,0 +1,17 @@
+import Crypto.Cipher.AES
+import requests
+
+k=input("输入密码URL:")
+t=input("输入文件准确地址:")
+name=input("为解密文件的命名：")
+key=requests.get(k).content
+print(f"密码为{key}")
+print("正在解密中，请等待")
+def decryptor(key,text):
+    de=Crypto.Cipher.AES.new(key,IV=b"0000000000000000",mode=Crypto.Cipher.AES.MODE_CBC)
+    return de.decrypt(text)
+f=open(fr"{t}","rb")
+k=open(fr"E:\下载\{name}","wb")
+k.write(decryptor(key,f.read()))
+print("解密完成，解密文件存入E盘下载文件夹！")
+a=input()
